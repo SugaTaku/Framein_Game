2092572846d587202fe323185e11a5d91fef9e88
SugaTaku <sugasugat0311@gmail.com>
2018-06-20 21:54:20 +0900

    チュートリアルを参考に基盤づくり(ジャンプ実装まで)

--- FrameinUnity/Assets/Scripts/Player2D.cs
+++ FrameinUnity/Assets/Scripts/Player2D.cs
@@ -5,15 +5,52 @@ public class Player2D : MonoBehaviour
 {
 
     public float speed = 4f; //歩くスピード
+    public float jumpPower = 700; //ジャンプ力
+    public LayerMask groundLayer; //Linecastで判定するLayer
+
     public GameObject mainCamera;
     private Rigidbody2D rigidbody2D;
     private Animator anim;
+    private bool isGrounded; //着地判定
 
     void Start()
     {
         //各コンポーネントをキャッシュしておく
         anim = GetComponent<Animator>();
         rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //Linecastでユニティちゃんの足元に地面があるか判定
+        isGrounded = Physics2D.Linecast(
+        transform.position + transform.up * 1,
+        transform.position - transform.up * 0.05f,
+        groundLayer);
+        //スペースキーを押し、
+        if (Input.GetKeyDown("space"))
+        {
+            //着地していた時、
+            if (isGrounded)
+            {
+                //Dashアニメーションを止めて、Jumpアニメーションを実行
+                anim.SetBool("Dash", false);
+                anim.SetTrigger("Jump");
+                //着地判定をfalse
+                isGrounded = false;
+                //AddForceにて上方向へ力を加える
+                rigidbody2D.AddForce(Vector2.up * jumpPower);
+            }
+        }
+        //上下への移動速度を取得
+        float velY = rigidbody2D.velocity.y;
+        //移動速度が0.1より大きければ上昇
+        bool isJumping = velY > 0.1f ? true : false;
+        //移動速度が-0.1より小さければ下降
+        bool isFalling = velY < -0.1f ? true : false;
+        //結果をアニメータービューの変数へ反映する
+        anim.SetBool("isJumping", isJumping);
+        anim.SetBool("isFalling", isFalling);
     }
 
     void FixedUpdate()
