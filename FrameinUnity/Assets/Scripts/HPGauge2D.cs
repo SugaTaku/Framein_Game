6ce69bd79c25814dafa70b0cdb2dd84bebea32f6
SugaTaku <sugasugat0311@gmail.com>
2018-06-21 21:58:11 +0900

    チュートリアルを参考に基盤づくり(回復アイテム実装まで)

--- FrameinUnity/Assets/Scripts/HPGauge2D.cs
+++ FrameinUnity/Assets/Scripts/HPGauge2D.cs
@@ -16,4 +16,15 @@ public class HPGauge2D : MonoBehaviour
         //RectTransformのサイズを取得し、マイナスする
         rt.sizeDelta -= new Vector2(0, ap);
     }
+
+    public void LifeUp(int hp)
+    {
+        //RectTransformのサイズを取得し、プラスする
+        rt.sizeDelta += new Vector2(0, hp);
+        //最大値を超えたら、最大値で上書きする
+        if (rt.sizeDelta.y > 240f)
+        {
+            rt.sizeDelta = new Vector2(51f, 240f);
+        }
+    }
 }
---
+++ FrameinUnity/Assets/Scripts/Item2D.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item2D : MonoBehaviour
+{
+
+    public int healPoint = 20;
+    public HPGauge2D lifeScript;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //ユニティちゃんと衝突した時
+        if (col.gameObject.tag == "Player")
+        {
+            //LifeUpメソッドを呼び出す　引数はhealPoint
+            lifeScript.LifeUp(healPoint);
+            //アイテムを削除する
+            Destroy(gameObject);
+        }
+    }
+}
